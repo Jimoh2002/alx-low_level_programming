adba46a908eb84af549316e3d51e9b05812a7bd0
“jimoh2002” <mrjimohsodiq0@gmail.com>
2022-03-11 00:45:25 -0800

    haargh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
-  *main - entry point
-  *
-  *Return: Always 0 (success)
-  */
+*main - entry point
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"programming is like builing a multilingual puzzle"):
-return (0);
+puts("Programming is like buuilding a multilingual puzzle")
+return (0)
 }
-
