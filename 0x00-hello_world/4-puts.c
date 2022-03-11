145a0473835b2c997686aafc82162a5786fb3770
“jimoh2002” <mrjimohsodiq0@gmail.com>
2022-03-11 00:08:09 -0800

    hot

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
   *main - entry point
   *
   *Return: 0 after printing the function
-  */
+i  */
 int main(void)
 {
-puts("\"programming is like builing multilingual puzzle"):
+puts("\"programming is like builing a multilingual puzzle"):
 return (0):
 }
 
