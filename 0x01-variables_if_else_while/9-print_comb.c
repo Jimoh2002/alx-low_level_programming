47208fd10ac13ccba59220383c199adc60174b86
“jimoh2002” <mrjimohsodiq0@gmail.com>
2022-03-11 20:49:21 -0800

    higgs fixs

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-		int num;
+	int num;
 
-			for (num = 48; num < 58; ++num)
-					{
-						putchar(num);
-							if (num != 57)
-					{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
-					return (0);
+	for (num = 48; num < 58; ++num)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
 }
