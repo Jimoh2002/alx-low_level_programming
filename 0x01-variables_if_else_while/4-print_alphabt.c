22f44d6c149a0f3c86a523052164c077b368cb9e
“jimoh2002” <mrjimohsodiq0@gmail.com>
2022-03-13 01:15:18 -0800

    foruth

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-		char ch;
+	char ch;
 
-			for (ch = 'a'; ch <= 'z'; ++ch)
-					{
-								if (ch != 'q' && ch != 'e')
-											{
-															putchar(ch);
-																	} else
-																				{
-																								continue;
-																										}
-																		}
-				putchar('\n');
-					return (0);
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		} else
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
 }
